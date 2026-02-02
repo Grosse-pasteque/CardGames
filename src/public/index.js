f41b5357db8523d313541657f43f747a757ceb7f
Grosse-pasteque <grossepasteque.gamer34@gmail.com>
2026-02-02 19:46:26 +0100

    feat(menu): realtime status of every game

--- src/public/index.js
+++ src/public/index.js
@@ -1,17 +1,30 @@
 console.log('Loaded main');
 
+let games;
+const gameItems = {};
+const realtime = new WebSocket('ws://localhost:8888/realtime');
+realtime.onmessage = message => updateGameItem(JSON.parse(message.data));
+
 (async () => {
-    const realtime = new WebSocket('ws://localhost:8888/realtime');
     const rk = await fetch('http://localhost:8888/games');
-    const games = await rk.json();
-    for (const game of games) {
-        list.innerHTML += `
-<div>
-    <b>${game.name}</b>
-    <br>
-    Queue: ${game.waiting}
-    <br>
-    Games: ${game.running}
-</div>`;
-    }
+    games = await rk.json();
+    games.forEach(updateGameItem);
+    list.addEventListener('click', e => {
+        if (e.target.tagName !== 'DIV') return;
+        location.href = location.origin + '/make/' + e.target.dataset.id;
+        /*
+        if (e.target.classList.toggle('expand')) {
+            updateGameDetails(e.target.dataset.id);
+            e.target.appendChild(gameDetails)
+        }
+        */
+    })
 })();
+
+function updateGameItem(game) {
+    const item = (gameItems[game.id] ||= list.appendChild(document.createElement('div')));
+    item.dataset.id = game.id;
+    item.innerHTML = `<b>${game.name}</b><br>Queue: ${game.waiting}<br>Games: ${game.running}`;
+}
+
+// <div style="float: right;"><button>Create</button><br><br><button>Queue</button></div>
