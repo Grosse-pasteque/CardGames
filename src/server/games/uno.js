974bac1dfcd2f9ced2100d581772ba00abe9e352
Grosse-pasteque <grossepasteque.gamer34@gmail.com>
2026-02-04 05:20:24 +0100

    refractor: adjust uno game server side code to ease integration with Rooms

--- src/server/games/uno.js
+++ src/server/games/uno.js
@@ -1,17 +1,3 @@
-/*
-- pile + melangée
-- 
-// value , couleur , type
-
-// NUMBERS + plus2 + skips + changeDir
-const CARDS = Array((10 + 2 + 2 + 2) * 4 )
-const pile = CARDS.shuffle()
-*/
-
-// { type, value, color }
-
-let ID = [];
-
 const CardType = {
     NUMBER: 0,
     PLUS_TWO: 1,
@@ -32,59 +18,61 @@ function number(value, color) {
     return { type: CardType.NUMBER, value, color };
 }
 
+const DECK = [];
+for (const color of [CardColor.RED, CardColor.YELLOW, CardColor.GREEN, CardColor.BLUE]) {
+    DECK.push(number(0, color));
+    for (let value = 1; value <= 9; value++)
+        DECK.push(number(value, color), number(value, color));
+    for (const type of [CardType.PLUS_TWO, CardType.SKIPS, CardType.CHANGE_DIRECTION])
+        DECK.push({ type, value: 20, color }, { type, value: 20, color });
+}
+for (let i = 0; i < 4; i++)
+    DECK.push({ type: CardType.JOKER, value: 50, color: CardColor.BLACK }, { type: CardType.PLUS_FOUR, value: 50, color: CardColor.BLACK });
+
+const CARDS_COUNT = DECK.length
+const DECK_IDS = Array(CARDS_COUNT).fill().map((_, i) => i);
+
+
 class Game {
     constructor(players) {
         this.players = players; // { nickname, hand }
-        this.pioche = [];
+        this.pile = [];
+        this.discard = [...DECK_IDS];
         this.turn = 0;
         this.direction = 1; // -1
-        this.currentCard = null;
-        this.reset();
+        this.top = null;
     }
     start() {
-        // distribuer
+        // shuffle
+        for (let i = 0; i < CARDS_COUNT; i++)
+            this.pile.push(this.discard.splice(Math.floor(Math.random() * CARDS_COUNT), 1));
+        // distribute
         for (let player in this.players) {
             while (player.hand.length < 7) {
-                player.hand.push(this.pioche.pop());
+                player.hand.push(this.pile.pop());
             }
         }
-        this.currentCard = this.pioche.pop();
-    }
-    reset() {
-        const deck = [];
-        for (const color of [CardColor.RED, CardColor.YELLOW, CardColor.GREEN, CardColor.BLUE]) {
-            deck.push(number(0, color));
-            for (let value = 1; value <= 9; value++)
-                deck.push(number(value, color), number(value, color));
-            for (const type of [CardType.PLUS_TWO, CardType.SKIPS, CardType.CHANGE_DIRECTION])
-                deck.push({ type, value: 20, color }, { type, value: 20, color });
-        }
-        for (let i = 0; i < 4; i++)
-            deck.push({ type: CardType.JOKER, value: 50, color: CardColor.BLACK }, { type: CardType.PLUS_FOUR, value: 50, color: CardColor.BLACK });
-        
-        while (deck.length)
-            this.pioche.push(deck.splice(Math.floor(Math.random() * deck.length), 1));
+        this.top = this.pile.pop();
     }
     play(player, card) {
-        // redefini currentCard in function of what this player plays
+        // redefine top in function of what this player plays
         if (
-            this.currentCard.type === card.type ||
-            this.currentCard.value === card.value ||
-            this.currentCard.color === card.color ||
+            this.top.type === card.type ||
+            this.top.value === card.value ||
+            this.top.color === card.color ||
             card.type === CardType.JOKER || card.type === CardType.PLUS_FOUR
         ) {
-            const nextPlayer = this.players[this.turn + 1]; // c'est pas [this.turn += this.direction] ici aussi?
+            const nextPlayer = this.players[this.turn + 1];
             switch (card.type) {
-                // parce que je les entends parler en chinois
                 case CardType.PLUS_TWO:
                     if (!nextPlayer.hand.some(card => card.type === CardType.PLUS_TWO || card.type === CardType.JOKER)) {
-                        nextPlayer.hand.push(this.pioche.pop(), this.pioche.pop());
+                        nextPlayer.hand.push(this.pile.pop(), this.pile.pop());
                         this.turn += this.direction;
                     }
                     break;
                 case CardType.PLUS_FOUR:
                     if (!nextPlayer.hand.some(card => card.type === CardType.PLUS_FOUR)) {
-                        nextPlayer.hand.push(this.pioche.pop(), this.pioche.pop(), this.pioche.pop(), this.pioche.pop());
+                        nextPlayer.hand.push(this.pile.pop(), this.pile.pop(), this.pile.pop(), this.pile.pop());
                         this.turn += this.direction;
                     }
                     break;
